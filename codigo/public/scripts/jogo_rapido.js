56522a742e421cd74149eec69c2fa86abe332ab1
rianpuc <redlopes@sga.pucminas.br>
2024-06-23 07:49:52 -0300

    logica feita so falta bloquear os botao

--- codigo/public/scripts/jogo_rapido.js
+++ codigo/public/scripts/jogo_rapido.js
@@ -6,7 +6,68 @@ if (usuarioLogado == false) {
 const userId = usuarioLogado === false ? '-1' : localStorage.getItem('usuarioCorrente');
 const currentUserObj = JSON.parse(userId);
 const id = currentUserObj.id;
-
+const modosPorJogo = {
+    'CS2': ['Competitivo', 'Braço Direito'],
+    'League of Legends': ['Ranqueada Solo/Duo', 'Ranqueada Flexivel'],
+    'Valorant': ['Ranqueada'],
+    'Rainbow Six Siege': ['Ranqueada'],
+    'Apex Legends': ['Battle Royale', 'Ranqueada'],
+    'Dota 2': ['Competitivo']
+};
+const niveisPorJogo = {
+    'CS2': [
+    { value: 'grey', text: '0 - 4999' },
+    { value: 'light_blue', text: '5000 - 9999' },
+    { value: 'blue', text: '10000 - 14999' },
+    { value: 'purple', text: '15000 - 19999' },
+    { value: 'pink', text: '20000 - 24999' },
+    { value: 'red', text: '25000 - 29999' },
+    { value: 'yellow', text: '30000+' }
+    ],
+    'League of Legends': {
+    'Ranqueada Solo/Duo': [
+    { value: 'low', text: 'Ferro IV - Prata I'},
+    { value: 'low-med', text: 'Ferro IV - Ouro I'},
+    { value: 'med', text: 'Prata IV - Platina I'},
+    { value: 'med-high', text: 'Ouro IV - Esmeralda I'},
+    { value: 'high', text: 'Platina IV - Diamante III'},
+    { value: 'highest', text: 'Diamante IV - Diamante I'},
+    { value: 'master', text: 'Diamante I - Mestre'}
+    ],
+    'Ranqueada Flexivel': [
+    { value: 'default', text: 'Ferro IV - Diamante I'},
+    { value: 'high', text: 'Platina 4 - Challenger'}
+    ]},
+    'Valorant': [
+    { value: 'low', text: 'Ferro 1 - Prata 3' },
+    { value: 'low-med', text: 'Prata 1 - Ouro 3'},
+    { value: 'med', text: 'Ouro 1 - Platina 3'},
+    { value: 'med-high', text: 'Platina 1 - Diamante 1'},
+    { value: 'high', text: 'Diamante 1 - Imortal 1'},
+    { value: 'highest', text: 'Imortal 1 - Radiante'}
+    ],
+    'Rainbow Six Siege': [
+    { value: 'low', text: '0 - 2200'},
+    { value: 'med', text: '2200 - 3400'},
+    { value: 'high', text: '3400 - 4400'},
+    { value: 'highest', text: '4400+'}
+    ],
+    'Apex Legends': {
+    'Battle Royale': [
+    { value: 'default', text: 'Livre'},
+    ],
+    'Ranqueada': [
+    { value: 'low', text: 'Rookie IV - Silver I'},
+    { value: 'med', text: 'Silver IV - Platinum I'},
+    { value: 'high', text: 'Platinum IV - Master'},
+    { value: 'highest', text: 'Master - Apex Predator'}
+    ]},
+    'Dota 2': [
+    { value: 'low', text: '0 - 2499' },
+    { value: 'med', text: '2500 - 4999' },
+    { value: 'high', text: '5000+' }
+    ]
+}
 fetch('http://localhost:3000/usuarios/' + currentUserObj.id)
     .then(response => {
         if (!response.ok) {
@@ -29,6 +90,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const buscarPartidaBtn = document.getElementById('buscar-partida-btn');
 
     buscarPartidaBtn.addEventListener('click', buscarPartida);
+    jogoSelect.addEventListener('change', function() {
+        const jogoSelecionado = jogoSelect.value;
+        modoSelect.innerHTML = '<option value="" disabled selected hidden>Selecione um Modo</option>';
+        nivelSelect.innerHTML = '<option value="" disabled selected hidden>Selecione um Nível</option>';
+        if (jogoSelecionado) {
+            modosPorJogo[jogoSelecionado].forEach(modo => {
+                const option = document.createElement('option');
+                option.value = modo;
+                option.textContent = modo;
+                modoSelect.appendChild(option);
+            });
+            modoSelect.disabled = false;
+        }
+      modoSelect.addEventListener('change', function() {
+          const jogoSelecionado = jogoSelect.value;
+          const modoSelecionado = modoSelect.value;
+          nivelSelect.innerHTML = '<option value="">Selecione um Nível</option>';
+          if (jogoSelecionado && modoSelecionado) {
+              const niveis = niveisPorJogo[jogoSelecionado][modoSelecionado] || niveisPorJogo[jogoSelecionado];
+              niveis.forEach(nivel => {
+                  const option = document.createElement('option');
+                  option.value = nivel.value;
+                  option.textContent = nivel.text;
+                  nivelSelect.appendChild(option);
+              });
+              nivelSelect.disabled = false;
+          }
+      });
+    });
 
     async function buscarPartida() {
         const jogo = jogoSelect.value;
