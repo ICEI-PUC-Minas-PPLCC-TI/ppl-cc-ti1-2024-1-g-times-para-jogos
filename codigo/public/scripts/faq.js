5358a7081d820b8366ffdf625b301761eb8073b5
rianpuc <redlopes@sga.pucminas.br>
2024-06-22 22:25:41 -0300

    asddafs

--- codigo/public/scripts/faq.js
+++ codigo/public/scripts/faq.js
@@ -1,5 +1,5 @@
 const usuarioLogado = localStorage.getItem('usuarioCorrente') !== null;
-if (!usuarioLogado) {
+if (usuarioLogado) {
   const fotoPerfil = document.getElementById('foto_de_perfil');
   fotoPerfil.src = '../assets/images/default_profile.png';
 }
