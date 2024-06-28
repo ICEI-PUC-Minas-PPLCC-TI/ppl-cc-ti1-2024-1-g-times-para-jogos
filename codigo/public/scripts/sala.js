6b61ddfe683f11a656836101a6a1ed82871dff41
rianpuc <redlopes@sga.pucminas.br>
2024-06-28 14:22:49 -0300

    ADD: aba de report, scrollbar no chatbox

--- codigo/public/scripts/sala.js
+++ codigo/public/scripts/sala.js
@@ -2,6 +2,117 @@ var usuarioLogado = true;
 if (localStorage.getItem('usuarioCorrente') == "{}" || localStorage.getItem('usuarioCorrente') == null) {
   window.location.href = 'login.html';
 } 
+
+async function fetchSalaMessages(salaId) {
+  try{
+    const response = await fetch(`/salas/${salaId}`);
+    if(!response.ok){
+      throw new Error('Erro ao buscar as mensagens');
+    }
+    const sala = await response.json();
+    return sala.mensagens;
+  } catch (error) {
+    console.error('Erro ao buscar as mensagens da sala ', error);
+    return [];
+  }
+} //Vou usar isso para pegar as mensagens do usuario no report
+
+async function getUserMessages(salaId, userId) {
+  const mensagens = await fetchSalaMessages(salaId);
+  console.log('Mensagens da sala:', mensagens); 
+  const userMensagens = mensagens
+    .filter(mensagem => {
+      return mensagem.autorId === userId;
+    })
+    .map(mensagem => mensagem.mensagem);
+  return userMensagens;
+}
+
+function openModal() {
+  document.getElementById('reportModal').style.display = 'block';
+}
+
+function closeModal() {
+  document.getElementById('reportModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById('reportModal');
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
+document.querySelector('.close').onclick = closeModal;
+
+let currentReportUserId = null;
+let currentReportUserName = null;
+let currentReportUserProfilePhoto = null;
+let currentSalaId = null;
+
+function reportPlayer(userId, userName, userProfilePhoto, salaId) {
+  currentReportUserId = userId;
+  currentReportUserName = userName;
+  currentReportUserProfilePhoto = userProfilePhoto;
+  currentSalaId = salaId;
+  document.querySelector('.reportingPlayerName').textContent = `${userName}`;
+  openModal();
+}
+
+async function submitReport(reportType) {
+  const reportDetails = {
+    userId: currentReportUserId,
+    reportType: reportType,
+    timestamp: new Date().toISOString(),
+    salaId: currentSalaId
+  };
+
+  switch(reportType) {
+    case 1:
+      reportDetails.offensiveNick = currentReportUserName;
+      break;
+    case 2:
+      reportDetails.verbalAbuseMessages = await getUserMessages(currentSalaId, currentReportUserId);
+      break;
+    case 3:
+      reportDetails.offensivePhotoURL = currentReportUserProfilePhoto;
+      break;
+    case 4:
+      reportDetails.hackReport = true;
+      break;
+    case 5:
+      reportDetails.smurfingReport = true;
+      break;
+    default:
+      alert('Tipo de reporte inválido.');
+      return;
+  }
+  sendReport(reportDetails);
+  closeModal();
+}
+
+function sendReport(reportDetails) {
+  fetch(`/reports`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reportDetails)
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (!data.success) {
+      alert('Report enviado com sucesso!');
+    } else {
+      alert('Falha ao enviar reporte. Tente novamente mais tarde.');
+    }
+  })
+  .catch(error => {
+    console.error('Erro ao enviar report:', error);
+    alert('Ocorreu um erro. Tente novamente mais tarde.');
+  });
+}
+
   const urlParams = new URLSearchParams(window.location.search);
   const userId = usuarioLogado === false ? '-1' : localStorage.getItem('usuarioCorrente');
   const currentUserObj = JSON.parse(userId);
@@ -61,7 +172,8 @@ if (localStorage.getItem('usuarioCorrente') == "{}" || localStorage.getItem('usu
             if (user) {
               const listItem = document.createElement('li');
               listItem.innerHTML = `<img src="${user.profilePhoto}" alt="Foto de ${user.nome}" style="width: 30px; height: 30px;"><span class="user-name-text">${user.login}</span>
-              ${(currentUserObj.login === sala.dono && user.login !== sala.dono) ? `<span class="kick-icon" onclick="kickPlayer('${salaId}', '${userId}')">✖</span>` : ''}`;
+              ${(currentUserObj.login === sala.dono && user.login !== sala.dono) ? `<span class="kick-icon" onclick="kickPlayer('${salaId}', '${userId}')">✖</span>` : ''}
+              ${(currentUserObj.login !== user.login) ? `<span class="report-icon" onclick="reportPlayer('${userId}', '${user.login}', '${user.profilePhoto}', '${salaId}')">!</span>` : ''}`;
               jogadoresList.appendChild(listItem);
             }
             if (currentUserObj.login === sala.dono) {
@@ -174,7 +286,12 @@ if (localStorage.getItem('usuarioCorrente') == "{}" || localStorage.getItem('usu
       
           function formatarHorario(data) {
             const date = new Date(data);
-            return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+            const dia = date.getDate();
+            const mes = date.getMonth() + 1;
+            const ano = date.getFullYear();
+            const horas = date.getHours();
+            const minutos = ('0' + date.getMinutes()).slice(-2);
+            return `${dia}/${mes}/${ano} ${horas}:${minutos}`;
         }
 
             function sendMessage() {
@@ -267,8 +384,5 @@ if (localStorage.getItem('usuarioCorrente') == "{}" || localStorage.getItem('usu
                   })
                   .catch(error => console.error('Erro ao carregar as mensagens:', error));
           }
-      
-      
           loadMessages();
-          setInterval(loadMessages, 3000);
       });
